1781e2eb372594a8f5a73ace547e682c30a68616
David Nicholas <Dnicholas@bitovi.com>
2024-01-18 21:22:03 -0800

    Docker configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
+const remotes = (isServer) => {
+  return {
+    header: isServer
+      ? `header@http://header:3001/_next/static/ssr/remoteEntry.js`
+      : `header@http://localhost:3001/_next/static/chunks/remoteEntry.js`,
+    services: isServer
+      ? `services@http://services:3002/_next/static/ssr/remoteEntry.js`
+      : `services@http://localhost:3002/_next/static/chunks/remoteEntry.js`,
+  };
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "standalone",
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "shell",
+        filename: "static/chunks/remoteEntry.js",
+        remotes: remotes(options.isServer),
+        shared: {},
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
